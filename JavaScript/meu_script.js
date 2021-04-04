e9ba4972208e729ff1f49bc37573bf2a3e9ac906
Hugo Barreto <74006806+hbferreira1@users.noreply.github.com>
2021-04-04 00:05:23 -0300

    Add files via upload

---
+++ JavaScript/meu_script.js
@@ -0,0 +1,10 @@
+alert('Olá, seja bem-vindo ao curso!')
+document.getElementById('nome').value = 'Oi'
+//comentário de uma linha.
+
+/*
+	Comentário de múltiplas linhas
+	Esse tipo de comentário permite
+	que várias linhas sejam encapsuladas
+	em um mesmo comentário.
+*/
